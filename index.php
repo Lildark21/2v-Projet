c46a80380c9dea04ce92b6ab942d9eb06a550ac7
Lildark21 <lilianorrit@gmail.com>
2024-04-17 09:09:24 +0200

    e

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-
+<title>PSY LIB</title>
 <?php 
 include './composants/header.php'; 
 include 'data/db.php'
@@ -11,7 +11,7 @@ include 'data/db.php'
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css\index.css">
-    <title>PSI LIB</title>
+    
 </head>
 <body>
     <div class="contenu">
